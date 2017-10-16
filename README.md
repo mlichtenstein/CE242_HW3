dd04c68710d3a9261316dd0ed5e12e12602f7a31
Max Lichtenstein <mnlichtenstein@gmail.com>
2017-10-15 18:44:36 -0700

    added a bunch of testing code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CE242_HW3
+
+A description of the important files in this folder:
+
+dataToFeatures.py:
+ This contains functions for importing a CSV and turning it into features:
--- dataToFeatures.py
+++ dataToFeatures.py
@@ -1,12 +1,13 @@
 #  This module contains tools for getting features from a data file
 
-
+print('importing CountVectorizer...')
 from sklearn.feature_extraction.text import CountVectorizer
 
 def readHamSpamFile(filename):
     features = []
 
     f = open(filename, 'r')
+    print('reading data from ' + filename + '...')
     for line in f:
         try:
             (hamspam, subjectline) = line.split(',',1)
@@ -22,18 +23,45 @@ def readHamSpamFile(filename):
     return features
 
 if __name__=='__main__':
-    raw_features = readHamSpamFile('train.csv')
-    #print(raw_features)
+    print('\nbeginning dataToFeatures test...')
+    raw_features = readHamSpamFile('train1.csv')
+    print('got %d features' % (len(raw_features)))
+    print('here are the raw features 1:')
+    print(raw_features)
 
 #okay, that looks good.  Can we tokenize it???
 
-def tokenizeRawFeatures(raw_features):
+def tokenizeRawFeatures(raw_features, vectorizer = None):
+    '''A function that takes raw features( a list of tuples, (bool, string))
+    and optionally an extant vectorizer.
+    and returns (X, vectorizer)
+    where X is the set of tokenized features
+    and vectorizer is the vectorizer that produced that.'''
 
-    vectorizer = CountVectorizer()
+    #if no vectorizer is specified, create a new vectorizer:
+    if vectorizer == None:
+        print('creating a new vectorizer...')
+        vectorizer = CountVectorizer()
+
     corpus = [x[1] for x in raw_features]
-    #print(corpus)
     X = vectorizer.fit_transform(corpus)
-    print(X.toarray())
-    
+    return(X, vectorizer)
+
 if __name__=='__main__':
-    tokenizeRawFeatures(raw_features)
+    print('training raw features on a new vectorizer:')
+    X,vectorizer = tokenizeRawFeatures(raw_features)
+    print('here is the vectorized corpus 1:')
+    print(X)
+    print('here is the vocabulary of the vectorizer:')
+    print(vectorizer.vocabulary_)
+
+
+    #we might also want to run this on a different set of features, so let's see how that looks:
+
+    raw_features2 = readHamSpamFile('train2.csv')
+    X2 = tokenizeRawFeatures(raw_features2,vectorizer)
+
+    print('here are the raw features 2:')
+    print(raw_features)
+    print('here is the vectorized corpus 2:')
+    print(X)
